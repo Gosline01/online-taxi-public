60f6b1a959211e58e61ee57c9ad7ea83e40f532e
gosling <599332429@qq.com>
2024-04-27 11:05:56 +0800

    第一次提交

---
+++ api-passenger/src/main/java/com/gosling/apipassenger/controller/VerificationCodeController.java
@@ -0,0 +1,23 @@
+package com.gosling.apipassenger.controller;
+
+import com.gosling.apipassenger.request.VerificationCodeDTO;
+import com.gosling.apipassenger.service.VerificationCodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VerificationCodeController {
+
+    @Autowired
+    private VerificationCodeService verificationCodeService;
+
+    @GetMapping("/verification-code")
+    public String verificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO) {
+
+        String passengerPhone = verificationCodeDTO.getPassengerPhone();
+        System.out.println("接收到的手机号：" + passengerPhone);
+        return verificationCodeService.generateCode(passengerPhone);
+    }
+}
